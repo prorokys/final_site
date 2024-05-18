1fc70ed9b95d1aa7eaf8563939d80df7fcb4e6d7
ukrkaban <rseminenko@gmail.com>
2024-05-18 15:16:49 +0300

    postgresql data base connected & full text search

--- final_site/settings.py
+++ final_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "debug_toolbar",
     "main",
     "goods",
@@ -78,9 +79,17 @@ WSGI_APPLICATION = "final_site.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "home",
+        "USER": "home",
+        "PASSWORD": "Piona1986$",
+        "HOST": "Localhost",
+        "PORT": "5432",
     }
 }
 
